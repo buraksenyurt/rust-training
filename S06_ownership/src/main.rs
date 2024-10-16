2047c01dad0bc66d544b4d4989a03027dc17b5e9
buraksenyurt <burakselimsenyurt@gmail.com>
2024-10-16 14:03:05 +0300

    Worked on ownership and move topics.

---
+++ S06_ownership/src/main.rs
@@ -0,0 +1,106 @@
+fn main() {
+    {
+        // Kasıtlı bir scope başlangıcı
+        let player_score = 90;
+        println!("Player Score is {}", player_score);
+        let _title = String::from("Programming with Rust");
+        let _size = (640_f32, 480_f32);
+    } // Scope sonu
+
+    // println!("Player Score is {}",player_score); // Scope dışından owner_score'a erişilemez(dropped)
+
+    move_rule();
+    move_rule_with_clone();
+
+    /*
+       i32 gibi türler Copy trait'ini uygularlar. Bu tip boyutça küçük veri türleri
+       kopyalama ile taşındıklarından bir move ihlali söz konusu olmaz.
+       Copy ve Clone aslında birer Trait' tir ve belli veri türlerine uygulanır.
+       Clone, deep copy işlemi uyguladığından Copy operasyonuna göre maliyetlidir.
+    */
+    let number = 32u8;
+    let other_number = number;
+    println!("Number is {}", number);
+    println!("Other number is {}", other_number);
+
+    /*
+       Örneğin String türü otomatik bir kopyalama işlemi yapmaz. Açıkça klonlamak gerekir.
+       String veri türünün Rust tarafındaki implementasyonuna bakmakta yarar var.
+       Clone trait'ini uyguladığını açıkça görebiliriz.
+
+       Eğer first_name'i full_name'i oluşturmak için kullanırken clone'larsak move sorunu giderilir.
+       Yine de Clone operasyonunun maliyetli olduğunu unutmamamlıyız.
+    */
+    let first_name = String::from("Burak");
+    let full_name = first_name + " Selim Şenyurt";
+    println!("Full name is {}", full_name);
+    // println!("First name is {}", first_name); // Value used after being moved [E0382]
+
+    let book_title = String::from("Programming with Rust");
+    search_book(book_title); // sahiplik(ownership) metota geçer
+                             // Bu nedenle aşağıdaki satırda artık book_title kullanılamaz.
+                             // println!("Book title is {}", book_title); // Value used after being moved [E0382]
+
+    /*
+       Aşağıdak örnekte de, bir String değişken check_department fonksiyonuna gönderilmekte.
+       Fonksiyon söz konusu String değişkenin sahipliğini alıp bazı operasyonlar gerçekleştirdikten
+       sonra aynen geriye dönebilir.
+    */
+    let department = String::from("IT");
+    let department = check_department(department); // Sahiplik fonksiyondan geri alınıyor
+    println!("The department is {}", department);
+
+    /*
+        Birde Shallo Copy'yi baz alan değer türlerine bakalım.
+        f32'de stack bazlı bir veri türüdür ve Deep Copy yerine basit veri kopyalamasını kullanır (Copy)
+        Bu nedenle metoda geçen değer otomatik olarak kopyalanarak gider.
+        Dönüşünde tekrardan kullanılabilir. Özetle move ihlali olmaz.
+    */
+    let amount = 955.50f32;
+    send_money(amount);
+    println!("{} coin sent", amount);
+}
+
+fn send_money(money: f32) {
+    println!("{} coin is sending...", money);
+}
+
+fn check_department(department: String) -> String {
+    println!("Calculating defects for '{}' department", department);
+    department
+}
+
+/*
+   Bu fonksiyon parametre olarak gelen title değişkeninin sahipliğini de alır (ownership)
+   Dolayısıyla metottan çıkılırken scope kuralı gereği title değişkeni bellekten düşürülür.
+*/
+fn search_book(title: String) {
+    println!("Starting search for '{}'", title);
+}
+
+fn move_rule() {
+    // Ownership yasasında move ihlali için Vec tabanlı örnek
+    let names = vec![
+        "John".to_string(),
+        "Beatrice".to_string(),
+        "Zee".to_string(),
+    ];
+    let _names_again = names; // names vektör sahipliği names_again'e geçti. names artık yok.
+                              // println!("{:?}", names); // Value used after being moved [E0382]
+}
+
+fn move_rule_with_clone() {
+    /*
+        Ownership yasasında move ihlali clone operasyonu ile aşılabilir ancak;
+        Aşağıdaki gibi String türden veri içeren büyük bir vektör için clone operasyonunun
+        büyük bir maliyeti vardır.
+    */
+    let names = vec![
+        "John".to_string(),
+        "Beatrice".to_string(),
+        "Zee".to_string(),
+    ];
+    let names_again = names.clone(); // names vektörünün bir klonu oluşturulur ve bunun sahipliği names_again değişkenine verilir
+    println!("{:?}", names); // Bu durumda names halen kullanılabilirdir.
+    println!("{:?}", names_again);
+}
