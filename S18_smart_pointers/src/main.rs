34f0b7f366bcc62ff8fbe495de403eaa5e9976a5
buraksenyurt <burakselimsenyurt@gmail.com>
2024-11-15 16:21:47 +0300

    Added a Rc, RefCell scenario.

--- S18_smart_pointers/src/main.rs
+++ S18_smart_pointers/src/main.rs
@@ -13,14 +13,40 @@
            adresindeki verinin sahipliğini alırlar ve onu manipüle etmemize izin verirler)
        - Deref ve Drop trait’lerini implemente eden struct türleri olarak tasarlanabilirler
            (Yani kendi Smart Pointer modellerimizi tasarlayabiliriz)
+
+    Hangisi ne zaman?
+
+        Box ve RefCell birden fazla sahipliği tek bir veri üzerinde sağlarken,
+        Rc aynı veri üzerinden birden fazla sahiplik sunar.
+
+        Box immutable veya mutable ödünç alma (borrowing) için derleme zamanında kontrol sağlar.
+        Rc sadece immutable borrowing için derleme zamanında kontrol sağlar.
+        RefCell immutable veya mutable ödünç alma için runtime'da kontrol sağlar.
 */
 mod boxing;
+mod rc_scenario;
 
 use crate::boxing::*;
+use crate::rc_scenario::*;
 
 fn main() {
     simple_boxing();
     //recursive_data_model_with_error();
     recursive_sample();
-}
+    hello_rc();
+    // run_rc_with_error();
+    // run_rc_with_error_2();
 
+    /*
+        Alttaki senaryoda aslında farklı kullanım şekilleri de söz konusu olabilir.
+
+        - Sadece bir vektör üzerinde çalışma yapıyorsak RefCell<Vec<Player>> kullanımı yeterlidir.
+        - Vektörün paylaşımı söz konusu ise Rc<RefCell<Vec<Player>>> daha uygun olabilir.
+        - Hem vektörü hem de içindeki elemanları paylaşmamız gerekirse Rc<Vec<RefCell<Player>>>
+            daha iyi bir çözüm olabilir.
+
+        Tabii şunu uutmamamak lazım ki hem Rc hem de RefCell kullanımı nedeniyle
+        runtime maliyeti daha yüksektir (Zir burada referans sayımı ve mutasyon kontrolleri yapılır)
+     */
+    run_rc();
+}
---
+++ S18_smart_pointers/src/rc_scenario.rs
@@ -0,0 +1,165 @@
+/*
+   Heap üzerindeki alokasyonlar Multiple Ownership gerektiği durumlarda Rc kullanılabilir.
+   RC referansları sayan bir kabiliyete sahiptir.
+   Birde Arc<T> vardır yani Atomic Referans Counting. Bunu daha çok paylaşımın thread'ler arasında
+   güvenli şekilde sağlanmasını istediğimiz durumlarda ele alırız.
+*/
+use std::cell::{Ref, RefCell};
+use std::rc::Rc;
+
+pub fn hello_rc() {
+    /*
+       p1, p2 ve p2 aynı veriyi işaret eden ve stack' de duran pointer'lardır.
+    */
+    let p1 = Rc::new(String::from("Some values on the heap"));
+    let p2 = p1.clone();
+    let p3 = p2.clone();
+
+    println!("p1={:?}", p1);
+    println!("p2={:?}", p2);
+    println!("p3={:?}", p3);
+}
+
+/*
+   İlk olarak aşağıdaki senaryoyu göz önüne alalım.
+   Burada Player veri modeli yine kendi türünden bir vektör kullanarak arkadaş listesini tutuyor.
+*/
+// #[derive(Debug)]
+// struct Player {
+//     id: u32,
+//     name: String,
+//     friends: Vec<Player>,
+// }
+//
+// impl Player {
+//     fn new(id: u32, name: &str) -> Self {
+//         Player {
+//             id,
+//             name: name.to_string(),
+//             friends: Vec::new(),
+//         }
+//     }
+//
+//     fn add_friend(&mut self, friend: Player) {
+//         self.friends.push(friend);
+//     }
+//
+//     fn print(&self) {
+//         println!("{}'s friends:", self.name);
+//         for friend in &self.friends {
+//             println!("  {} (ID: {})", friend.name, friend.id);
+//         }
+//     }
+// }
+//
+// pub fn run_rc_with_error() {
+//     let mut steve = Player::new(1, "Stivi Vondır");
+//     let lord = Player::new(2, "Lord veyda");
+//     let anakin = Player::new(3, "Anakin");
+//
+//     steve.add_friend(lord); // lord' un sahipliği add_friend sonrası steve' e taşındı
+//     steve.add_friend(anakin);
+//
+//     steve.print();
+//
+//     println!("Lord veyda's ID: {}", lord.id); // Dolayısıyla burada value moved here hatası alınır
+// }
+
+/*
+   İkinci olarak Rc kullandığımız senaryoya bakalım ama bu seferde kendimiz
+   error[E0596]: cannot borrow data in an `Rc` as mutable
+   hatasına götüreceğiz.
+*/
+// #[derive(Debug)]
+// struct Player {
+//     id: u32,
+//     name: String,
+//     friends: Vec<Rc<Player>>,
+// }
+//
+// impl Player {
+//     fn new(id: u32, name: &str) -> Rc<Self> {
+//         Rc::new(Player {
+//             id,
+//             name: name.to_string(),
+//             friends: Vec::new(),
+//         })
+//     }
+//
+//     fn add_friend(self: &Rc<Self>, friend: Rc<Player>) {
+//         self.friends.push(friend);
+//     }
+//
+//     fn print(&self) {
+//         println!("{}'s friends:", self.name);
+//         for friend in self.friends.iter() {
+//             println!("  {} (ID: {})", friend.name, friend.id);
+//         }
+//     }
+// }
+//
+// pub fn run_rc_with_error_2() {
+//     let steve = Player::new(1, "Stivi Vondır");
+//     let lord = Player::new(2, "Lord veyda");
+//     let anakin = Player::new(3, "Anakin");
+//
+//     steve.add_friend(Rc::clone(&lord));
+//     steve.add_friend(Rc::clone(&anakin));
+//
+//     steve.print();
+//
+//     println!("Lord Veyda's ID: {}", lord.id);
+// }
+
+/*
+    Yukarıdaki senaryoda bir Player'ın kendisini tutan bir Vector nesnesine ihtiyacı olmuştur.
+    Bunun için Vec'ün Rc<Player> kullanılması yolu tercih edilmiştir. Ancak bu
+    özellikle add_friends metodunda vektör içeriğine mutable erişmeyi gerektirir. Bu nedenle
+    vektöre referansının da mutable olarak ele alınabilmesi gerekir. Normalde birden fazla sahip
+    varken mutable olma hali derleme hatasına yol açabilir. RefCell kullanımı ile bunu runtime'a
+    taşırız. Yani ownership kontrolünü runtime tarafında işleriz.
+
+    Bu kullanımda borrow_mut ve borrow metot çağrımları söz konusudur. Bu çağrımlar esasında
+    RefCell türevli Ref<T> ve RefMut<T> trait'lerini kullanır.
+ */
+
+#[derive(Debug)]
+struct Player {
+    id: u32,
+    name: String,
+    friends: RefCell<Vec<Rc<Player>>>,
+}
+
+impl Player {
+    fn new(id: u32, name: &str) -> Rc<Self> {
+        Rc::new(Player {
+            id,
+            name: name.to_string(),
+            friends: RefCell::new(Vec::new()),
+        })
+    }
+
+    fn add_friend(self: &Rc<Self>, friend: Rc<Player>) {
+        self.friends.borrow_mut().push(friend);
+    }
+
+    fn print(&self) {
+        println!("{}'s friends:", self.name);
+        for friend in self.friends.borrow().iter() {
+            println!("  {} (ID: {})", friend.name, friend.id);
+        }
+    }
+}
+
+pub fn run_rc() {
+    let steve = Player::new(1, "Stivi Vondır");
+    let lord = Player::new(2, "Lord veyda");
+    let anakin = Player::new(3, "Anakin");
+
+    steve.add_friend(Rc::clone(&lord));
+    steve.add_friend(Rc::clone(&anakin));
+
+    steve.print();
+
+    println!("Lord Veyda's ID: {}", lord.id);
+}
