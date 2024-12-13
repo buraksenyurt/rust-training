0afe0337dcf8f988fd1d99f68aae25fd777907d3
buraksenyurt <burakselimsenyurt@gmail.com>
2024-12-14 00:16:11 +0300

    Used arguments on procedural macros.

--- S21_macros/src/main.rs
+++ S21_macros/src/main.rs
@@ -9,7 +9,8 @@
    Bu projedeki test metotlarında declarative macro örnekleri yer almaktadır.
    Aynı zamanda wtime isimli bir procedural macro kullanım örneği de mevcuttur.
 */
-use proc_macros::work_time_effort;
+use log::info;
+use proc_macros::{log_execution, work_time_effort, work_time_effort_log};
 
 mod model;
 mod samples;
@@ -19,12 +20,58 @@ mod without_macro;
 #[work_time_effort]
 fn find_total() {
     let mut total = 0;
-    for i in 0..100 {
+    for i in 0..1000 {
         total += i;
     }
     println!("Result: {}", total);
 }
 
+/*
+   Attribute türevli procedural makrolara parametre de geçilebilir.
+   Argümanlar, macrodaki ilk parametredeki TokenStream üzerinden yakalanır.
+*/
+#[work_time_effort_log("info")]
+fn find_another_total() {
+    let mut total = 0;
+    for i in 0..1_000 {
+        total += i;
+    }
+    println!("Result: {}", total);
+}
+
+struct Logger;
+
+impl Logger {
+    pub fn log(&self, msg: &str) {
+        info!("{}", msg);
+    }
+}
+
+#[log_execution(Logger)]
+fn sum() {
+    let mut total = 0;
+    for i in 0..1_000 {
+        total += i;
+    }
+    println!("Result: {}", total);
+}
+
+/*
+    Log çıktılarını görmek için uygulamayı linux platformunda
+
+        RUST_LOG=info cargo run
+
+    ile,
+    Windows platformunda Powershell' de ise
+
+        set RUST_LOG=info && cargo run
+
+    ile çalıştırabiliriz.
+ */
 fn main() {
-    let total = find_total();
+    env_logger::init();
+
+    let _total = find_total();
+    let _another_total = find_another_total();
+    let _sum = sum();
 }
--- proc_macros/src/lib.rs
+++ proc_macros/src/lib.rs
@@ -1,7 +1,7 @@
 extern crate proc_macro;
 use proc_macro::TokenStream;
 use quote::quote;
-use syn::{parse_macro_input, ItemFn};
+use syn::{parse_macro_input, Expr, ItemFn, LitStr};
 
 #[proc_macro_attribute]
 pub fn work_time_effort(_attr: TokenStream, item: TokenStream) -> TokenStream {
@@ -15,6 +15,51 @@ pub fn work_time_effort(_attr: TokenStream, item: TokenStream) -> TokenStream {
             #fn_block
             let duration = start.elapsed();
             println!("Total execution time {}: {:?}", stringify!(#fn_name), duration);
+        }
+    };
+
+    expanded.into()
+}
+
+#[proc_macro_attribute]
+pub fn work_time_effort_log(attr: TokenStream, item: TokenStream) -> TokenStream {
+    let log_level = parse_macro_input!(attr as LitStr).value();
+
+    let input = parse_macro_input!(item as ItemFn);
+    let fn_name = &input.sig.ident;
+    let fn_block = &input.block;
+
+    let expanded = quote! {
+        fn #fn_name() {
+            let start = std::time::Instant::now();
+            #fn_block
+            let duration = start.elapsed();
+            match #log_level {
+                "info" => println!("[INFO] Total execution time {}: {:?}", stringify!(#fn_name), duration),
+                "debug" => println!("[DEBUG] Total execution time {}: {:?}", stringify!(#fn_name), duration),
+                "error" => eprintln!("[ERROR] Total execution time {}: {:?}", stringify!(#fn_name), duration),
+                _ => println!("Unknown log level: {}", #log_level),
+            }
+        }
+    };
+
+    expanded.into()
+}
+
+#[proc_macro_attribute]
+pub fn log_execution(attr: TokenStream, item: TokenStream) -> TokenStream {
+    let logger_expr = parse_macro_input!(attr as Expr);
+
+    let input = parse_macro_input!(item as ItemFn);
+    let fn_name = &input.sig.ident;
+    let fn_block = &input.block;
+
+    let expanded = quote! {
+        fn #fn_name() {
+            let start = std::time::Instant::now();
+            #fn_block
+            let duration = start.elapsed();
+            #logger_expr.log(&format!("Execution time for {}: {:?}", stringify!(#fn_name), duration));
         }
     };
 
