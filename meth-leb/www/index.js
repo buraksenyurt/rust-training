a262b494c541198169afaeedc11a38a67ccb89b5
buraksenyurt <burakselimsenyurt@gmail.com>
2025-01-12 15:13:16 +0300

    Refactored frontend

--- meth-leb/www/index.js
+++ meth-leb/www/index.js
@@ -9,11 +9,29 @@ async function main() {
     const context = canvas.getContext("2d");
     const dropdown = document.getElementById("examples");
 
+    function drawAxes() {
+        context.beginPath();
+        context.strokeStyle = "gray";
+        context.lineWidth = 1;
+
+        context.moveTo(0, 300);
+        context.lineTo(800, 300);
+
+        context.moveTo(400, 0);
+        context.lineTo(400, 600);
+
+        context.stroke();
+        context.closePath();
+    }
+
+    drawAxes();
+
     function draw(a, b, c, x_min, x_max, steps) {
 
         const points = calculate_parabola(a, b, c, x_min, x_max, steps);
 
         context.clearRect(0, 0, canvas.width, canvas.height);
+        drawAxes();
         context.beginPath();
         context.moveTo((points[0] - x_min) * 50, 300 - points[1] * 50);
 
