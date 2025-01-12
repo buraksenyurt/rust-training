cc939b449fe6c5c7ad87a840a2548716aab244d2
buraksenyurt <burakselimsenyurt@gmail.com>
2025-01-12 13:53:26 +0300

    Refactored frontend

--- meth-leb/www/index.js
+++ meth-leb/www/index.js
@@ -1,21 +1,15 @@
 import init, {calculate_parabola} from "../pkg/meth_leb.js";
 
 async function main() {
+
     await init();
 
-    const form = document.getElementById('form');
-    const canvas = document.getElementById('canvas');
-    const context = canvas.getContext('2d');
+    const form = document.getElementById("form");
+    const canvas = document.getElementById("canvas");
+    const context = canvas.getContext("2d");
+    const dropdown = document.getElementById("examples");
 
-    form.addEventListener('submit', (event) => {
-        event.preventDefault();
-
-        const a = parseFloat(document.getElementById('a').value);
-        const b = parseFloat(document.getElementById('b').value);
-        const c = parseFloat(document.getElementById('c').value);
-        const x_min = parseFloat(document.getElementById('x_min').value);
-        const x_max = parseFloat(document.getElementById('x_max').value);
-        const steps = parseInt(document.getElementById('steps').value);
+    function draw(a, b, c, x_min, x_max, steps) {
 
         const points = calculate_parabola(a, b, c, x_min, x_max, steps);
 
@@ -30,9 +24,25 @@ async function main() {
         }
 
         context.stroke();
+    }
+
+    dropdown.addEventListener("change", (event) => {
+        const [a, b, c, x_min, x_max, steps] = event.target.value.split(",").map(Number);
+        draw(a, b, c, x_min, x_max, steps);
+    });
+
+    form.addEventListener("submit", (event) => {
+        event.preventDefault();
+
+        const a = parseFloat(document.getElementById("a").value);
+        const b = parseFloat(document.getElementById("b").value);
+        const c = parseFloat(document.getElementById("c").value);
+        const x_min = parseFloat(document.getElementById("x_min").value);
+        const x_max = parseFloat(document.getElementById("x_max").value);
+        const steps = parseInt(document.getElementById("steps").value);
+
+        draw(a, b, c, x_min, x_max, steps);
     });
 }
 
-main()
-    .then(() => console.log("Parabola processed successfully!"))
-    .catch((error) => console.error("Error on main function", error));
+main().catch((error) => console.error("Error on loading wasm module.", error));
